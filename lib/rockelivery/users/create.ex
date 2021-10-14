c01218f0427e181aaa4ff79258c104cbc4e19cce
Rodrig Barreto <rodrigonbarreto@gmail.com>
2021-10-14 16:01:28 +0200

    refactoring create.call

--- lib/rockelivery/users/create.ex
+++ lib/rockelivery/users/create.ex
@@ -3,9 +3,14 @@ defmodule Rockelivery.Users.Create do
   alias Rockelivery.ViaCep.Client
 
   def call(%{"cep" => cep} = params) do
-    case Client.get_cep_info(cep) do
-      {:ok, _cep_info} -> create_user(params)
-      {:error, _result} = error -> error
+    changeset = User.changeset(params)
+    with {:ok, %User{}} <- User.build(changeset),
+         {:ok, _cep_info} <- Client.get_cep_info(cep),
+         {:ok, %User{}} = user <- Repo.insert(changeset) do
+      user
+    else
+      {:error, %Error{}} = error -> error
+      {:error, result} -> {:error, Error.build(:bad_request, result)}
     end
   end
 
